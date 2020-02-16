a15d5cb7f47533f2b46e6f26727ba3622b9ba74f
WMXPY <wm@mengw.io>
2020-02-16 14:58:19 -0600

    add slide help method

---
+++ src/slide.ts
@@ -0,0 +1,10 @@
+/**
+ * @author WMXPY
+ * @namespace Time
+ * @description Slide
+ */
+
+export const slideCurrentDate = (control: Date = new Date()): Date => {
+
+    return control;
+};
---
+++ test/unit/slide.test.ts
@@ -0,0 +1,23 @@
+/**
+ * @author WMXPY
+ * @namespace Time
+ * @description Slide
+ * @override Unit Test
+ */
+
+import { expect } from "chai";
+import * as Chance from "chance";
+import { slideCurrentDate } from "../../src/slide";
+
+describe('Given [Slide] helper functions', (): void => {
+
+    const chance: Chance.Chance = new Chance('time-slide');
+
+    it('should be able create current date', (): void => {
+
+        const now: Date = new Date();
+        const currentDate: Date = slideCurrentDate(now);
+
+        expect(currentDate.getTime()).to.be.equal(now.getTime());
+    });
+});
