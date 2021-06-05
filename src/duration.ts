81bfae04bda6b6d2acbc02204bba11a15fae747b
WMXPY <wm@mengw.io>
2021-06-05 13:30:08 -0500

    implement firs duration function

--- src/duration.ts
+++ src/duration.ts
@@ -3,3 +3,25 @@
  * @namespace Time
  * @description Duration
  */
+
+export type DurationConfig = {
+
+    readonly hours?: number;
+    readonly minutes?: number;
+    readonly seconds?: number;
+};
+
+export class Duration {
+
+    public static of(config: DurationConfig): Duration {
+
+        return new Duration(config);
+    }
+
+    private milliseconds: number;
+
+    private constructor(config: DurationConfig) {
+
+        this.milliseconds = config.seconds as number;
+    }
+}
